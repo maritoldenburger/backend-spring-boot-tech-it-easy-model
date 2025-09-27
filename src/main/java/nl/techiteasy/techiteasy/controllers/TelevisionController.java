f1ecc49dae7e52009873411de0838f98e5cc68d7
moldenburger <maritoldenburger@hotmail.com>
2025-09-27 18:02:28 +0200

    TelevisionRepository aangemaakt, TelevisionController aangepast

--- src/main/java/nl/techiteasy/techiteasy/controllers/TelevisionController.java
+++ src/main/java/nl/techiteasy/techiteasy/controllers/TelevisionController.java
@@ -1,42 +1,83 @@
 package nl.techiteasy.techiteasy.controllers;
 
+import nl.techiteasy.techiteasy.models.Television;
+import nl.techiteasy.techiteasy.repositories.TelevisionRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/televisions")
 public class TelevisionController {
 
-    private List<String> televisionDataBase = new ArrayList<>();
+    private final TelevisionRepository repos;
+
+    public TelevisionController(TelevisionRepository repos) {
+        this.repos = repos;
+    }
 
     @GetMapping
-    public ResponseEntity<List<String>> getAllTelevisions() {
-        return ResponseEntity.ok(televisionDataBase);
+    public ResponseEntity<List<Television>> getAllTelevisions() {
+        return ResponseEntity.ok(repos.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> getTelevision(@PathVariable int id) {
-        return ResponseEntity.ok(televisionDataBase.get(id));
+    public ResponseEntity<Television> getTelevision(@PathVariable Long id) {
+        Optional<Television> optionalTelevision = this.repos.findById(id);
+        if (optionalTelevision.isPresent()) {
+            return ResponseEntity.ok(optionalTelevision.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    public ResponseEntity<String> createTelevision(@RequestBody String television) {
-        televisionDataBase.add(television);
-        return ResponseEntity.created(null).build();
+    public ResponseEntity<Television> createTelevision(@RequestBody Television television) {
+        Television savedTelevision = this.repos.save(television);
+        return ResponseEntity.created(null).body(savedTelevision);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Object> updateTelevision(@PathVariable int id, @RequestBody String television) {
-        televisionDataBase.set(id, television);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Television> updateTelevision(@PathVariable Long id, @RequestBody Television television) {
+        Optional<Television> optionalTelevision = this.repos.findById(id);
+        if (optionalTelevision.isPresent()) {
+            Television updatedTelevision = optionalTelevision.get();
+
+            updatedTelevision.setType(television.getType());
+            updatedTelevision.setBrand(television.getBrand());
+            updatedTelevision.setName(television.getName());
+            updatedTelevision.setPrice(television.getPrice());
+            updatedTelevision.setAvailableSize(television.getAvailableSize());
+            updatedTelevision.setRefreshRate(television.getRefreshRate());
+            updatedTelevision.setScreenType(television.getScreenType());
+            updatedTelevision.setScreenQuality(television.getScreenQuality());
+            updatedTelevision.setSmartTv(television.isSmartTv());
+            updatedTelevision.setWifi(television.isWifi());
+            updatedTelevision.setVoiceControl(television.isVoiceControl());
+            updatedTelevision.setHdr(television.isHdr());
+            updatedTelevision.setBluetooth(television.isBluetooth());
+            updatedTelevision.setAmbiLight(television.isAmbiLight());
+            updatedTelevision.setOriginalStock(television.getOriginalStock());
+            updatedTelevision.setSold(television.getSold());
+
+            this.repos.save(updatedTelevision);
+            return ResponseEntity.ok(updatedTelevision);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteTelevision(@PathVariable int id) {
-        televisionDataBase.set(id, null);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Void> deleteTelevision(@PathVariable Long id) {
+        Optional<Television> optionalTelevision = this.repos.findById(id);
+        if (optionalTelevision.isPresent()) {
+            this.repos.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
+
 }
---
+++ src/main/java/nl/techiteasy/techiteasy/repositories/TelevisionRepository.java
@@ -0,0 +1,7 @@
+package nl.techiteasy.techiteasy.repositories;
+
+import nl.techiteasy.techiteasy.models.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
+}
